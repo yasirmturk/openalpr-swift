ede385b859e8d7c7f2b5927a3af303ca0627968b
MBP <yasirmturk@gmail.com>
2018-01-05 11:41:59 +0500

    Initial Commit

---
+++ openalpr-swift/openalpr_swift.h
@@ -0,0 +1,19 @@
+//
+//  openalpr_swift.h
+//  openalpr-swift
+//
+//  Created by Yasir M Turk on 05/01/2018.
+//  Copyright © 2018 Yasir M Türk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for openalpr_swift.
+FOUNDATION_EXPORT double openalpr_swiftVersionNumber;
+
+//! Project version string for openalpr_swift.
+FOUNDATION_EXPORT const unsigned char openalpr_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <openalpr_swift/PublicHeader.h>
+
+
